1f2408ee04d2105d2d6cd67a4b4ee2335c44eb5c
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-10-29 03:07:24 -0300

    Implement OTP verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 // import { Button } from '@/components/ui/button'
 import PatientForm from '@/components/forms/PatientForm';
+import PasskeyModal from '@/components/PasskeyModal';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+export default  function Home({ searchParams }: SearchParamProps) {
+
+  const isAdmin =  searchParams?.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen ">
-
-      {/* TODO: OTP Verification | PassKy Modal */}
+      {isAdmin && <PasskeyModal />}
 
       <section className='remove-scrollbar container my-auto'>
         <div className="sub-container max-w-[496px]">
@@ -17,14 +20,14 @@ export default function Home() {
 
           <div className='text-14-regular mt-20 flex justify-between'>
             <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
-            <Link href={"/?dmin=true"} className="text-green-500">
+            <Link href={"/?admin=true"} className="text-green-500">
               Admin
             </Link>
           </div>
         </div>
       </section>
 
-      <Image src={'/assets/images/onboarding-img.png'} alt='Patient' height={1000} width={1000} className='side-img max-w-[50%]'/>
+      <Image src={'/assets/images/onboarding-img.png'} alt='Patient' height={1000} width={1000} className='side-img max-w-[50%]' />
 
     </div>
   );
